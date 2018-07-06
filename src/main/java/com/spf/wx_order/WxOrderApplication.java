9d14f78b73facb2561354f68d6e140c5d2135030
shupf@stargoto.com <shupf>
2018-07-06 15:53:03 +0800

    微信点餐系统init

---
+++ src/main/java/com/spf/wx_order/WxOrderApplication.java
@@ -0,0 +1,12 @@
+package com.spf.wx_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxOrderApplication.class, args);
+    }
+}
